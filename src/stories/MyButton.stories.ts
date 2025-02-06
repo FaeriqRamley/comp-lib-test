48e85bd84c5b3c54ea6bc2e959a0fda796151480
Faeriq Ramley <mohamad.faeriq.ramley@gmail.com>
2025-02-06 10:55:05 +0800

    added button to stories

--- src/stories/MyButton.stories.ts
+++ src/stories/MyButton.stories.ts
@@ -1,16 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
-
-import { MyButton, myButtonConfig } from "./MyButton";
+import { MyButton } from "./MyButton";
 
 const meta: Meta<typeof MyButton> = {
-    title: "Actual/Button",
+    title: "Atoms/Button",
     tags: ["autodocs"],
     component: MyButton,
-    argTypes: Object.fromEntries(
-        Object.entries(myButtonConfig.variants).map(([key, val]) => {
-            return [key, { type: "select", options: Object.keys(val) }];
-        })
-    ),
 };
 
 export default meta;
@@ -19,5 +13,26 @@ type Story = StoryObj<typeof meta>;
 export const Primary: Story = {
     args: {
         variant: "primary",
+        children: "Click Me!",
     },
 };
+
+// argTypes: {
+//     ...Object.fromEntries(
+//         Object.entries(myButtonConfig.variants).map(([key, val]) => {
+//             return [
+//                 key,
+//                 {
+//                     controls: {
+//                         type: "select",
+//                         options: Object.keys(val),
+//                     },
+//                     description: `Available options:\n${Object.keys(
+//                         val
+//                     ).join(" | ")}`,
+//                 },
+//             ];
+//         })
+//     ),
+//     ref: { table: { disable: true } }, // Hide ref from autodocs
+// },
--- src/stories/MyButton.tsx
+++ src/stories/MyButton.tsx
@@ -2,42 +2,34 @@ import { JSX } from "react";
 import { cn } from "../cn";
 import { cva, VariantProps } from "class-variance-authority";
 
-// Define a type for the button variants
-type ButtonVariants = {
-  variant: "primary" | "secondary";
-  animation: "bouncy" | "stringy";
-};
-
-export const myButtonConfig = {
-  // default variants define the default values for each variant. MUST BE ADDED FOR EVERY VARIANT
-  defaultVariants: {
-    variant: "primary" as ButtonVariants["variant"],
-    animation: "stringy" as ButtonVariants["animation"],
-  },
-  // Self explanatory design variants. These variants will appear as component props
-  variants: {
-    /** Main variant 
-     * @type "primary" | "secondary" */
-    variant: {
-      primary: "bg-primary-base text-primary-foreground",
-      secondary: "bg-secondary-base text-secondary-foreground",
-    },
-    /** Animator variant */
-    animation: {
-      bouncy: "",
-      stringy: "",
-    },
-  },
-}
-
 const myButtonVariants = cva(
   // Base class string that applies to all variants
-  "px-7 py-2 rounded-2xl cursor-pointer ",
-  myButtonConfig
+  "px-7 py-2 rounded-2xl cursor-pointer transition-colors duration-500 ",
+  {
+    // default variants define the default values for each variant. MUST BE ADDED FOR EVERY VARIANT
+    defaultVariants: {
+      variant: "primary",
+      animation: "stringy",
+    },
+    // Self explanatory design variants. These variants will appear as component props
+    variants: {
+      /** Main variant 
+       * @type "primary" | "secondary" */
+      variant: {
+        primary: "bg-primary-base text-primary-foreground active:bg-red-300",
+        secondary: "bg-secondary-base text-secondary-foreground",
+      },
+      /** Animator variant */
+      animation: {
+        bouncy: "",
+        stringy: "",
+      },
+    },
+  }
 );
 
 
-interface MyButtonProps
+export interface MyButtonProps
   extends Omit<React.ComponentProps<"button">, "variant" | "animation">,
   VariantProps<typeof myButtonVariants> {
   ref?: React.Ref<HTMLButtonElement>;
